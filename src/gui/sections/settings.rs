02dcd07aeac64e55c3e652e7ae7623a8132a82b3
chironjit <chironjit@gmail.com>
2025-01-31 20:27:44 +0100

    Added some basic guides in the settings section

--- src/gui/sections/settings.rs
+++ src/gui/sections/settings.rs
@@ -7,5 +7,32 @@ use crate::model::State;
 use crate::updates::Message;
 
 pub fn view(state: &State) -> Element<Message> {
-    column![text("Just a placeholder")].spacing(10).into()
+    let content = column![
+                                        
+        container(
+            column![
+                container(text("Settings").size(20))
+                    .padding([0, 10])
+                    .align_x(Horizontal::Left), 
+                text("1. Use the Profiles section to set power profiles").size(12),
+                text("2. Use the Custom Overrides section to set custom power profiles").size(12),
+                text("3. Enter power limits in miliWatts(mW)").size(12),
+                text("4. Enter temp limits in Celsius(°C)").size(12),
+            ]
+            .spacing(10)
+        )
+        .style(card_style())
+        .width(Length::Fill)
+        .padding(10)
+    ]
+    .spacing(20)
+    .padding(10)
+    .width(Length::Fill);
+
+    container(content)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .align_x(Horizontal::Left)
+        .align_y(Vertical::Top)
+        .into()
 }
